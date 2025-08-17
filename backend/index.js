777a32610964075fb1c868cf5adfa6b6e7d32c6c
mahaboobjilani <inthiyazshaik833@gmail.com>
2025-08-17 16:57:03 +0530

    added index

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,9 @@
-// const express=require("express");
-// const app=express();
-
-// const cors=require('cors')
-
-// const dotenv=require("dotenv").config();
-// const connectMongoDb=require("./config/connectionDb")
-// const PORT=process.env.PORT || 3000
-
-// connectMongoDb()
-
-// app.use(express.json());
-// app.use(cors());
-// app.use(express.static('public'));
-
-// app.use("/recipe",require('./routes/recipe.js'))
-// app.use("/",require('./routes/user.js'))
-
-// app.listen(PORT, () => {
-//   console.log(`Server started at port ${PORT}`);
-// });
-
 const express = require("express");
-const app = express();
-
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const connectMongoDb = require("./config/connectionDb");
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectMongoDb();
@@ -63,4 +41,3 @@ app.use("/", require("./routes/user.js"));
 app.listen(PORT, () => {
   console.log(`🚀 Server started at port ${PORT}`);
 });
-
