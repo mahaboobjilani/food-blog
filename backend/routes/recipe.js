48f89f37c611e350b4d2e65f96cf86676e96346f
mahaboobjilani <inthiyazshaik833@gmail.com>
2025-08-17 17:01:42 +0530

    modified the code

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const {getRecipes, getRecipe,addRecipe,editRecipe,deleteRecipe,upload}= require("../controller/recipe");
-// const verifyToken=require("../middleware/auth.js")
-// const router=express.Router();
-
-// router.get("/allRecipies",getRecipes);
-// router.get("/:id",getRecipe);
-// router.post("/", upload.single("file"), verifyToken,addRecipe);
-
-// router.put("/:id",upload.single("file"),editRecipe);
-// router.delete("/:id",deleteRecipe);
-
-// module.exports = router;
-
 const express = require("express");
 const {
   getRecipes,
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,20 +1,5 @@
-// const express = require("express");
-// const{userSignup,userLogin,getUser}=require("../controller/user.js")
-// const router=express.Router();
-
-// router.post("/signup",userSignup)
-// router.post("/login",userLogin)
-// router.get("/user/:id",getUser)
-
-
-// module.exports = router;
-
 const express = require("express");
-const {
-  userSignup,
-  userLogin,
-  getUser
-} = require("../controller/user.js");
+const { userSignup, userLogin, getUser } = require("../controller/user.js");
 
 const router = express.Router();
 
